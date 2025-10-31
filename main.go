3e7e6ea4c05e4aef779e3f00c101828332feaa8d
SeyedMohammadMahdi <st.mohammad1380@gmail.com>
2025-10-31 13:41:32 +0330

    [UPDATE] added the badgerdb and also completed the get handler also added the put route

--- main.go
+++ main.go
@@ -1,22 +1,88 @@
+//   ____ ___ _   _       ____  ____
+//  / ___|_ _| \ | |     |  _ \| __ )
+// | |  _ | ||  \| |_____| | | |  _ \
+// | |_| || || |\  |_____| |_| | |_) |
+//  \____|___|_| \_|     |____/|____/
+
+// this project is going to implement a simple database using gin framework as the webserver backbone
 package main
 
 import (
+	// "bytes"
+	// "encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 
+	"github.com/dgraph-io/badger/v4"
 	"github.com/gin-gonic/gin"
 )
 
+type Data struct {
+	Key   string          `json:"key" binding:"required"`
+	Value any `json:"value" binding:"required"`
+}
+
 func main() {
+
+	db, err := badger.Open(badger.DefaultOptions("./data"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
 	router := gin.Default()
-	
+
 	router.GET("/objects/:id", func(c *gin.Context) {
 
 		id := c.Param("id")
 		c.Status(200)
+		var response any
+		err := db.View(func(txn *badger.Txn) error {
+			item, err := txn.Get([]byte(id))
 
-		// retrieve the data if it exists
+			if err != nil {
+				return err
+			}
 
-		c.JSON(http.StatusOK, gin.H{"id": id})
+			// var err error
+			response, err = item.ValueCopy(nil)
+
+			if err != nil {
+				return err
+			}
+
+			return nil
+		})
+
+		if err == badger.ErrKeyNotFound {
+			// c.Status(http.StatusNotFound)
+			c.JSON(http.StatusNotFound, gin.H{
+				"message": "not found",
+				"id": id,
+			})
+			return
+		}
+		c.JSON(http.StatusOK, response)
+	})
+
+	
+	router.PUT("/objects", func(c *gin.Context) {
+		var data Data
+		if err := c.ShouldBindJSON(&data); err != nil {
+			c.Status(400)
+			return
+		}
+		fmt.Println(data.Value)
+
+		// var pretty bytes.Buffer
+		// if err := json.Indent(&pretty, data.Value, "", "  "); err != nil {
+		// 	// fallback if raw JSON is invalid
+		// 	fmt.Printf("Key: %s, Value (raw): %s\n", data.Key, string(data.Value))
+		// } else {
+		// 	fmt.Printf("Key: %s\nValue:\n%s\n\n", data.Key, pretty.String())
+		// }
+
 	})
 
 	router.Run()
